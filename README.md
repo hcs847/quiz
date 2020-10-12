5717be9be5f2f7eb9a40f6d0d002c464c8f329b7
Hila Caspi <caspi.home@gmail.com>
2020-10-11 23:14:41 -0700

    finalizing the quiz

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# quiz
-A basic multiple answers quiz for JavaScript.
+# JavaScript quiz
+
+## Description
+
+A basic multiple choice questions quiz for JavaScript.
+This application lists JavaScript questions with 3 possible answers to select from.
+The score is timebased, the quicker it takes to solve, the higher the score while incorrect answers deduct 10 points from the score.
+The highest scores are saved to local storage.
+
+### URL: https://github.com/hcs847/quiz
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // setup variables
 var quizButtonEl = document.querySelector("#quiz-btn");
 var quizHeader = document.querySelector("#quiz-hdr");
-var questionsEl = document.querySelector("#questions-section");
 var answersElm = document.querySelector("#answer-choice");
 var counterEl = document.querySelector("#score");
-var timeLeft = 75;
+var timeLeft = 105;
 var scoreCountdown;
-
+var questionsEl = document.createElement("div");
+var submitButtonEl = document.querySelector("#initials-btn");
 
 // create an array of objects for questions and answers
 var questionsOb = [{
@@ -15,40 +15,142 @@ var questionsOb = [{
             text: "Document Object Model",
             correct: true
         }, {
-            text: "a12",
+            text: "Document Objective Modeling",
             correct: false
         }, {
-            text: "a13",
+            text: "Domain Object Mail",
             correct: false
         }]
     },
     {
         question: "What is a Callback function? ",
         answers: [{
-            text: "a21",
+            text: "A function that calls back the DOM",
             correct: false
         }, {
             text: "A function passed as an argument into another function",
             correct: true
         }, {
-            text: "a23",
+            text: "A function that calls back another file",
             correct: false
         }]
     },
     {
-        question: "Difference property and method setup",
+        question: "What is the difference between property and method setup",
         answers: [{
-            text: "a31",
+            text: "Properties are set up without quatation marks",
             correct: false
         }, {
-            text: "A property setup is: .property = value, while for Methods it’s: method(‘ ’)",
+            text: "A property setup syntax is: .property = value, while for Methods it’s: method( )",
             correct: true
         }, {
-            text: "a33",
+            text: "Methods are setup with at the begining of the code",
             correct: false
         }]
     },
+    {
+        question: "How do you print “Hello World” to the console? ",
+        answers: [{
+            text: 'console.log("Hello World")',
+            correct: true
+        }, {
+            Text: 'console.log.Hello World',
+            correct: false
+        }, {
+            text: 'alert.Hello.World',
+            correct: false
+        }]
+    },
+    {
+        question: "Which of the following are the Logical operators for Not, Or, And?",
+        answers: [{
+            text: "x, ?, & ",
+            correct: false
+        }, {
+            text: " ! , || , && ",
+            correct: true
+        }, {
+            text: " !!, |, # ",
+            correct: false
+        }]
+    },
+    {
+        question: "Which one of the values is NOT Falsy? ",
+        answers: [{
+            text: "Null",
+            correct: false
+        }, {
+            text: "’False’",
+            correct: true
+        }, {
+            text: "0",
+            correct: false
+        }]
+    },
+    {
+        question: "What is the correct syntax of For Loops? ",
+        answers: [{
+            text: 'for (var i = 0 ; i < n ; i++ ) { };',
+            correct: true
+        }, {
+            Text: '(var i = 0 , i < n , i++ );',
+            correct: false
+        }, {
+            Text: '(var i = 0 &&i < n && i++ )',
+            correct: false
+        }]
+    },
+    {
+        question: "What Method is used to extract the number of items in an array? ",
+        answers: [{
+            text: "The extract function",
+            correct: false
+        }, {
+            text: "The length property",
+            correct: true
+        }, {
+            text: "The get method",
+            correct: false
+        }]
+    },
+    {
+        question: "How do you return the nth item of a string? ",
+        answers: [{
+            text: 'string.charAt(n)',
+            correct: true
+        }, {
+            text: '"string.nth()"',
+            correct: false
+        }, {
+            text: '"string(nth) ="',
+            correct: false
+        }]
+    },
+
+    {
+        question: "How do you concatenate strings in JavaScript? ",
+        answers: [{
+            text: " stringA && stringB && stringC",
+            correct: false
+        }, {
+            text: "stringA + stringB + stringC ",
+            correct: true
+        }, {
+            text: "stringA.stringB.sringC",
+            correct: false
+        }]
+    },
+
+
 ];
+
+var createQuizContainer = function () {
+    // creating a container for the quiz header
+    questionsEl.className = "card-quiz"
+    questionsEl.id = "questions-section"
+    bodyEl = document.getElementsByTagName("BODY")[0];
+    bodyEl.appendChild(questionsEl);
+};
 
 var startQuizHandler = function (event) {
     //  var testStart = event.target;
@@ -56,7 +158,8 @@ var startQuizHandler = function (event) {
 
     // start the timer which will be the score once stopped
     scoreCountdown = setInterval(timer, 1000);
-    // console.log("This is countdown " + countdown);
+
+    createQuizContainer();
     getQuestions(questionsOb, 0);
 };
 
@@ -91,12 +194,10 @@ var listingQuestions = function (i) {
         answerChoiceEl.appendChild(answersEl);
 
         if (j === 2) {
-
             answerChoiceEl.addEventListener("click", submitAnswerHandler);
         }
     }
 };
-
 
 var submitAnswerHandler = function (event) {
 
@@ -108,36 +209,41 @@ var submitAnswerHandler = function (event) {
     var divParEl = document.querySelector(
         "[answer-question-id='" + iValue + "']");
 
+    if (timeLeft === 0) {
+        questionDiv.remove();
+        clearInterval(scoreCountdown);
+        counterEl.remove();
+        questionsEl.innerHTML = "<p> Time is Up, please try again </p>";
+    }
+
     if (answer === "true") {
         var resultEl = document.createElement("p");
-        resultEl.textContent = "correct";
+        resultEl.textContent = "Correct";
         divParEl.appendChild(resultEl);
-
-
 
     } else if (answer === "false") {
         var resultEl = document.createElement("p");
-        resultEl.textContent = "incorrect";
+        resultEl.textContent = "Incorrect";
         divParEl.appendChild(resultEl);
+        timeLeft -= 10;
 
     }
-    if (iValue < 2) {
+    if (iValue < 9) {
         setTimeout(function () {
             questionDiv.remove();
             iValue++;
             getQuestions(questionsOb, iValue);
-        }, 800);
-    } else if (iValue === "2") {
+        }, 700);
+
+    } else if (iValue === "9") {
         questionDiv.remove();
         clearInterval(scoreCountdown);
-        saveScore();
-
+        counterEl.remove();
+        getInitials();
     }
 };
 
-
 // var countdown = setInterval(timer, 1000);
-
 var timer = function () {
     if (timeLeft >= 0) {
         counterEl.textContent = "Time: " + timeLeft;
@@ -145,14 +251,53 @@ var timer = function () {
     }
 };
 
-var saveScore = function () {
-    localStorage.setItem("highscore", (timeLeft + 1));
+var getInitials = function () {
+    var finalScore = timeLeft + 1;
+    questionsEl.innerHTML = "<p>All Done!<br />Your Final score is " + finalScore + "</p>"
+    var formEl = document.createElement("form");
+    formEl.method = "POST";
+    formEl.innerHTML =
+        "<div class='input-field'><label for='initials'>Please enter your Initials <br /></label><input type='text' name='initials' id='initials'/></div><div id='initials-button'></div><button id='initials-btn' type='button'>Submit</button>"
+    questionsEl.appendChild(formEl);
+    var submitButtonEl = document.querySelector("#initials-btn");
+
+    if (submitButtonEl) {
+        submitButtonEl.addEventListener("click", submitScoreHandler);
+    };
+
 };
 
-var getUserInitials = function () {
+var submitScoreHandler = function (event) {
+    event.preventDefault();
 
-}
+    var score = (timeLeft + 1);
+    localStorage.setItem("highscore", score);
+    var initials = document.querySelector("#initials").value;
+    localStorage.setItem("initials", initials);
 
+    savedInitials = localStorage.getItem("highscore");
+    savedScore = localStorage.getItem("initials");
+
+    savedHighScore = savedScore + ' - ' + savedInitials;
+
+
+    // render high scores
+    renderHighScore(savedHighScore);
+};
+
+var renderHighScore = function (savedHighScore) {
+
+    userInputEl = document.querySelector("#questions-section");
+    userInputEl.innerHTML = "<h2>High Scores<br /></h2><p>" + savedHighScore + "<br /></p><button id='clear-btn' type='button'>Go Back</button>";
+    var goBackButtonEl = document.querySelector("#clear-btn");
+
+    goBackButtonEl.addEventListener("click", clearButtonHandler);
+
+};
+
+var clearButtonHandler = function (event) {
+    location.reload();
+};
 
 
 // event handlers
